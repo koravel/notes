f3be6d40d837c974ebf01c3e60b4b300c2353637
Alex Dubovik <a.dubovik@simcord.com>
2019-09-03 16:26:38 +0300

    changed note adding method

--- js/main.js
+++ js/main.js
@@ -1,80 +1,116 @@
 var deleteButtonName = "delete-note-btn";
 var editButtonName = "edit-node-btn";
+var saveButtonName = "save-changes-btn";
+var contextMenu = "note-context-menu";
+var contextMenuButton = "note-context-menu-button";
+var titleInput = "note-title";
+var descriptionTextArea = "note-description";
+
 
 var addButton;
 
 var notes = [];
 
-document.body.onload = () => {
+document.addEventListener("DOMContentLoaded", event => {
+    addButton = document.getElementById("add-note-btn");
+    addButton.onclick = addNoteContainer;
 
     notes = JSON.parse(localStorage.getItem("notes"));
     if(notes != null) {
         notes.forEach(note => {
             noteContainer = addNoteContainer();
-            editNote(note);
+            
+            editContainer(noteContainer, note);
+
         });
     }
     else notes = [];
-
-    addButton = document.getElementById("add-note-btn");
-
-    addButton.onclick = addNoteContainer;
-}
+});
 
 document.body.onunload = () => {
     localStorage.setItem("notes", JSON.stringify(notes));
 }
 
-function editNoteContainer(noteContainer) {
-    noteContextMenu = document.getElementById("noteContextMenu");
+function editContainer(noteContainer, note) {
+    noteContainer.getElementsByClassName(titleInput)[0].value = note.title;
+    noteContainer.getElementsByClassName(descriptionTextArea)[0].value = note.description;
+}
 
-    noteContainer.innerHTML =
-    `<div class="note-title"></div>
-    <hr>
-    <div class="note-description"></div>
-    <input type=button class="btn btn-outline-danger ${delete_button_name}">`;
+function switchToEdit(noteContainer) {
+    noteTitle = noteContainer.getElementsByClassName("note-title")[0];
+    noteDescription = noteContainer.getElementsByClassName("note-description")[0];
 
-    document.getElementById("notes-container").insertBefore(noteContainer, addButton);
+    noteTitle.readOnly = false;
+    noteDescription.readOnly = false;
+    
+    noteTitle.className = "note-title-editing";
+    noteDescription.className = "note-description-editing";
+    
+    noteContainer.getElementsByClassName(saveButtonName)[0].style.visibility = "visible";
+}
+
+function switchToNote(noteContainer) {
+    noteTitle = noteContainer.getElementsByClassName("note-title-editing")[0];
+    noteDescription = noteContainer.getElementsByClassName("note-description-editing")[0];
+    
+    noteTitle.className = "note-title";
+    noteDescription.className = "note-description";
+    
+    noteContainer.getElementsByClassName(saveButtonName)[0].style.visibility = "hidden";
 }
 
 function addNoteContainer() {
     let noteContainer = document.createElement(`div`);
     noteContainer.className = "note-container";
     
-    noteContainer.innerHTML =
-    `<input type="text" class="note-title" placeholder="title...">
-    <hr>
-    <input type="text" class="note-description" placeholder="description...">
-    <div class="noteContextMenu">
-            <input class="btn btn-secondary dropdown-toggle-split" type="button" id="noteContextMenuBtton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" value="...">
-            <div class="dropdown-menu" aria-labelledby="noteContextMenuBtton">
+    noteContainer.innerHTML = 
+    `<div class=${contextMenu}>
+            <input class="btn btn-secondary dropdown-toggle-split" type="button" id="${contextMenuButton}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" value="...">
+            <div class="dropdown-menu" aria-labelledby="${contextMenuButton}">
                 <input type=button class="dropdown-item ${deleteButtonName}" value="delete">
                 <input type=button class="dropdown-item ${editButtonName}" value="edit">
             </div>
-    </div>`;
-
-    document.getElementById("notes-container").insertBefore(noteContainer, addButton);
+    </div>
+    <input type="text" class="note-title" placeholder="title..."></textArea>
+    <hr>
+    <div class="d-flex flex-column edit-container">
+        <textArea type="text" class="note-description" cols=25 rows=2 placeholder="description..."></textArea>
+        <input class="btn btn-secondary dropdown-toggle-split ${saveButtonName}" type="button" value="save">
+    </div>
+    `;
 
     noteContainer.onmouseover = () => {
-        noteContainer.getElementsByClassName("noteContextMenu")[0].style.visibility = "visible";
+        noteContainer.getElementsByClassName(contextMenu)[0].style.visibility = "visible";
     };
 
     noteContainer.onmouseout = () => {
-        noteContainer.getElementsByClassName("noteContextMenu")[0].style.visibility = "hidden";
+        noteContainer.getElementsByClassName(contextMenu)[0].style.visibility = "hidden";
     };
 
     noteContainer.getElementsByClassName(editButtonName)[0].onclick = event => {
-        
-        
-        noteContainer.getElementsByClassName("note-title")[0].readOnly = false;
-        noteContainer.getElementsByClassName("note-description")[0].readOnly = false;
+
+        switchToEdit(noteContainer);
     };
+
+    length = notes.length;
 
     noteContainer.getElementsByClassName(deleteButtonName)[0].onclick = event => {
         
+        notes.splice(notes[length - 1], 1);
 
         noteContainer.remove();
     };
+    
+    noteContainer.getElementsByClassName(saveButtonName)[0].onclick = () => {
+        switchToNote(noteContainer);
+        notes[length] = { 
+            id: length,
+            title: noteContainer.getElementsByClassName("note-title")[0].value,
+            description: noteContainer.getElementsByClassName("note-description")[0].value
+        };
+    };
+
+    document.getElementById("notes-container").insertBefore(noteContainer, addButton);
 
     return noteContainer;
 }
